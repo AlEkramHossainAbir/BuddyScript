273351b4e4e381b106b57793db3c67520e789296
Al- Ekram Hossain Abir <alabir65@gmail.com>
2025-11-24 12:15:56 +0600

    Update api.ts

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 
+console.log('API_URL:', API_URL);
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
