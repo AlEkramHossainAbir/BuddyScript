b3efda9fafbe296eace4f73ebd172ef95e2676b2
Al- Ekram Hossain Abir <alabir65@gmail.com>
2025-11-25 11:40:50 +0600

    err-fixed

--- client/components/RightSidebar.tsx
+++ client/components/RightSidebar.tsx
@@ -81,7 +81,7 @@ export default function RightSidebar() {
 													<div className="_feed_right_inner_area_card_ppl_side">
 														{friend.status === 'active' ? (
 															<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 14 14">
-																<rect width="12" height="12" x="1" y="1" fill="#0ACF83" stroke="#fff" stroke-width="2" rx="6" />
+																<rect width="12" height="12" x="1" y="1" fill="#0ACF83" stroke="#fff" strokeWidth="2" rx="6" />
 															</svg>
 														) : (
 															<span>{friend.time}</span>
