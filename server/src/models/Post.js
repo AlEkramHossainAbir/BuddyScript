909b971ed314b0da53e6b7e21b9834c6c25eea69
AlEkramHossainAbir <alabir65@gmail.com>
2026-04-02 08:33:21 +0600

    server created

---
+++ server/src/models/Post.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    default: null
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  reactions: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    type: {
+      type: String,
+      enum: ['like', 'love', 'haha', 'wow', 'sad', 'angry'],
+      default: 'like'
+    }
+  }]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Post', postSchema);
